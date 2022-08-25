9a4926b13be6d792b61d83412dc1de98a38e6db2
Yoon321 <tkdwls734@gmail.com>
2022-08-25 15:36:40 +0900

    initial setting

---
+++ pillpick/src/main/java/com/kbsc/pillpick/PillpickApplication.java
@@ -0,0 +1,13 @@
+package com.kbsc.pillpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillpickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PillpickApplication.class, args);
+    }
+
+}
---
+++ pillpick/src/test/java/com/kbsc/pillpick/PillpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbsc.pillpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillpickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
