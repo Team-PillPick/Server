1c9fd66c0773edcca39fd98feee5793f37bc1b63
ehdwoKIM <kinbell19@gmail.com>
2022-09-07 00:20:10 +0900

    [ADD] 홈 화면 조회 기능

--- pillpick/src/main/java/com/kbsc/pillpick/repository/MemberRepository.java
+++ pillpick/src/main/java/com/kbsc/pillpick/repository/MemberRepository.java
@@ -3,5 +3,7 @@ package com.kbsc.pillpick.repository;
 import com.kbsc.pillpick.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
--- pillpick/src/main/java/com/kbsc/pillpick/service/MemberService.java
+++ pillpick/src/main/java/com/kbsc/pillpick/service/MemberService.java
@@ -3,5 +3,6 @@ package com.kbsc.pillpick.service;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 }
