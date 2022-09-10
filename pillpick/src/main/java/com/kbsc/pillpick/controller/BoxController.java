cf4c8e082241a6fecb08bf725ba294f230c79ad2
Yoon321 <tkdwls734@gmail.com>
2022-09-10 15:28:05 +0900

    [ADD] 수거함 데이터 조회 api - 수거함 데이터 데이터베이스에 저장 - 수거함 데이터 조회

--- pillpick/src/main/java/com/kbsc/pillpick/controller/BoxController.java
+++ pillpick/src/main/java/com/kbsc/pillpick/controller/BoxController.java
@@ -1,7 +1,32 @@
 package com.kbsc.pillpick.controller;
 
+import com.kbsc.pillpick.common.response.BasicResponse;
+import com.kbsc.pillpick.service.BoxService;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("box")
 public class BoxController {
+
+    private final BoxService boxService;
+    @PostMapping()
+    public void saveBox() throws IOException, ParseException {
+        boxService.saveBox();
+    }
+
+    @GetMapping()
+    public ResponseEntity<BasicResponse> readAllBoxes(){
+        return boxService.readAllBoxes();
+    }
+
+
 }
