c2096c59d873c5d62a57bcfe1fc4b3bd33409884
김동재 <87058086+ehdwoKIM@users.noreply.github.com>
2022-09-12 14:04:05 +0900

    [ADD] Dto 추가

---
+++ pillpick/src/main/java/com/kbsc/pillpick/dto/memberDto/GetMemberResponseDto.java
@@ -0,0 +1,13 @@
+package com.kbsc.pillpick.dto.memberDto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class GetMemberResponseDto { //홈 정보
+
+    private String name;
+    private int count;
+    private int level;
+}
